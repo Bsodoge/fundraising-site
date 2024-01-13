7cf5b2f0797899def3710483584ff8c65e14ebf1
DR <kashhh4u@gmail.com>
2024-01-13 22:39:46 +0000

    Create tier box

---
+++ app/components/TierBox.tsx
@@ -0,0 +1,11 @@
+import styles from './TierBox.module.css';
+
+export default function TierBox() {
+    return (
+        <div className={styles.tier}>
+            <div>$Z</div>
+            <img src="" alt="tier3" />
+            <div>Lorem, ipsum dolor.</div>
+        </div>
+    )
+}
