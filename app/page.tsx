6c1b80cf2468cac50db0215d5cf914739a5d4328
DR <kashhh4u@gmail.com>
2024-01-13 22:36:34 +0000

    Structure page

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,42 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Button from './components/Button';
+import FaqContainer from './components/FaqContainer';
+import FaqFormContainer from './components/FaqFormContainer';
+import ProgressBar from './components/ProgressBar';
+import TierBox from './components/TierBox';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
+        <div className={styles.information}>
+          <h1 className={styles.header}>Create Ad Campaign in the Tram</h1>
+          <p className={styles.description}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque, doloribus. Harum, aspernatur iste dolorem optio ratione nostrum dolor vitae dolores at error maiores id corrupti ad tempore molestias eveniet accusantium, doloremque quidem magnam suscipit officia amet eum. Nobis eius sint ratione eveniet sapiente officiis, accusantium modi maxime veniam vero quis commodi corrupti, perferendis dolor esse aspernatur delectus atque nemo. Veritatis est consequuntur provident enim, vel nostrum, natus animi sunt accusantium perferendis, amet illum ipsam fugit nihil dolores autem inventore praesentium vero quos odit ad beatae error magni. Nostrum veniam necessitatibus ipsam, vitae numquam quod dolorum obcaecati placeat cum esse aspernatur reiciendis commodi unde, modi est sit laudantium ea ut, natus velit eligendi assumenda tempora accusamus in! Maxime dolore quasi sapiente odio itaque officiis minima voluptate est architecto ea debitis quas vero quam aut laboriosam, nulla inventore? Esse aliquam vero laborum eaque. Eos, consequatur. Nemo commodi debitis soluta quis, laudantium ut cum, nisi possimus hic nulla est, dolorem consequuntur aliquid id rerum cupiditate nihil libero neque saepe facilis delectus? Tenetur quia sit modi, esse odio ratione repellat perspiciatis recusandae rerum exercitationem fugit ipsum eos eius sapiente distinctio officia pariatur, perferendis delectus nostrum id temporibus tempore. Aspernatur fugiat placeat dolore excepturi incidunt!</p>
+          <h1 className={styles.header}>Frequently Asked Questions:</h1>
+          <ul className={styles.faq}>
+            <FaqContainer question='Lorem ipsum dolor sit.' answer='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum vero debitis earum ipsa labore sequi unde obcaecati, eos tempore fuga reiciendis ex nisi odio quis amet illo sint quidem asperiores.' />
+            <FaqContainer question='Lorem ipsum dolor sit.' answer='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum vero debitis earum ipsa labore sequi unde obcaecati, eos tempore fuga reiciendis ex nisi odio quis amet illo sint quidem asperiores.' />
+            <FaqContainer question='Lorem ipsum dolor sit.' answer='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum vero debitis earum ipsa labore sequi unde obcaecati, eos tempore fuga reiciendis ex nisi odio quis amet illo sint quidem asperiores.' />
+            <FaqContainer question='Lorem ipsum dolor sit.' answer='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum vero debitis earum ipsa labore sequi unde obcaecati, eos tempore fuga reiciendis ex nisi odio quis amet illo sint quidem asperiores.' />
+            <FaqFormContainer />
+          </ul>
+        </div>
+        <div className={styles.donation_container}>
+          <h2 className={styles.currently_raised}>
+            $X Raised of $Y Goal
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <ProgressBar />
+          <div className={styles.tier_container}>
+            <TierBox />
+            <TierBox />
+            <TierBox />
+          </div>
+          <form action="">
+            <label htmlFor="">Donation Amount:</label>
+            <input type="text" name="donation_amount" id="donation_amount" />
+          </form>
+          <Button button_text='Donate Now' />
+        </div>
       </div>
     </main>
   )
