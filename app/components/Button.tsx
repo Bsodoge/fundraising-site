6623a4d15db7c3a0a403dd0d70744ad68efc5cd7
DR <kashhh4u@gmail.com>
2024-01-14 01:49:42 +0000

    Add a tag with stripe link

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,7 +1,7 @@
 import styles from './Button.module.css'
 
-export default function FaqContainer({button_text} : IButton) {
+export default function FaqContainer({ button_text }: IButton) {
     return (
-        <button className={styles.button}>{button_text}</button>
+        <a className={styles.button} href="https://buy.stripe.com/test_00g14vgho0Ei5Ko5kk" target="_blank" rel="noopener noreferrer">{button_text}</a>
     )
 }
