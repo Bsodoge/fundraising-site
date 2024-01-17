80c7fa820eddf9de0f159bbd5b96f56849a9d55b
DR <kashhh4u@gmail.com>
2024-01-17 18:38:59 +0000

    Add donation popup

---
+++ app/components/DonationPopup.tsx
@@ -0,0 +1,9 @@
+import styles from "./DonationPopup.module.css";
+
+export default function DonationPopup(){
+    return(
+        <div className={styles.popup_container}>
+            $X Has been donated to the cause
+        </div>
+    )
+}
