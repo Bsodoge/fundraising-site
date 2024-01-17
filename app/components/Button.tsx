f043c224e7e2d1163c0b83bb98eec3d298bb4b88
DR <kashhh4u@gmail.com>
2024-01-17 08:15:53 +0000

    update button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,6 +2,6 @@ import styles from './Button.module.css'
 
 export default function FaqContainer({ button_text }: IButton) {
     return (
-        <a className={styles.button} href="https://buy.stripe.com/test_00g14vgho0Ei5Ko5kk" target="_blank" rel="noopener noreferrer">{button_text}</a>
+        <button className={styles.button}>{button_text}</button>
     )
 }
