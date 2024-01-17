35dc5bc71b1dacf83b82266b073da0b045bae9a6
DR <kashhh4u@gmail.com>
2024-01-17 08:16:39 +0000

    Update tierbox

--- app/components/TierBox.tsx
+++ app/components/TierBox.tsx
@@ -1,11 +1,13 @@
+import ITierBox from '../interfaces/ITierBox';
 import styles from './TierBox.module.css';
 
-export default function TierBox() {
+export default function TierBox({ tierName, tierRequirment, tierDescription, tierImage }: ITierBox) {
     return (
         <div className={styles.tier}>
-            <div>$Z</div>
-            <img src="" alt="tier3" />
-            <div>Lorem, ipsum dolor.</div>
+            <h3 className={styles.header}>{tierName}</h3>
+            <div>${tierRequirment}</div>
+            <img className={styles.image} src={tierImage} alt={tierName} />
+            <div>{tierDescription}</div>
         </div>
     )
 }
